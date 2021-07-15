accf3d4c9752af84d568dedd531222492647bf5a
wikomoko <mokoindonesia45@gmail.com>
2021-07-15 14:02:21 +0700

    activating view Binding on Splash SplashScreen.kt and activity_splash_screen.xml

--- app/src/main/java/com/example/candybar/SplashScreen.kt
+++ app/src/main/java/com/example/candybar/SplashScreen.kt
@@ -2,10 +2,13 @@ package com.example.candybar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.candybar.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+      binding=DataBindingUtil.setContentView(this,R.layout.activity_splash_screen)
     }
 }
