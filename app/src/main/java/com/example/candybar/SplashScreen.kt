59d71bccd6422be5dd6af2e8d90665336f11942a
wikomoko <mokoindonesia45@gmail.com>
2021-07-15 14:08:26 +0700

    Setting postDelayed on SplashScreen.kt

--- app/src/main/java/com/example/candybar/SplashScreen.kt
+++ app/src/main/java/com/example/candybar/SplashScreen.kt
@@ -1,7 +1,9 @@
 package com.example.candybar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.databinding.DataBindingUtil
 import com.example.candybar.databinding.ActivitySplashScreenBinding
 
@@ -10,5 +12,11 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
       binding=DataBindingUtil.setContentView(this,R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
     }
 }
